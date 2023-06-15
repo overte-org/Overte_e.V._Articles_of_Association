c20165fb32f16d6321e76f9d636e8033cc52e4bb
Julian Groß <julian.g@posteo.de>
2023-06-15 19:38:25 +0200

    Enable gettext UUIDs

--- source/conf.py
+++ source/conf.py
@@ -42,6 +42,8 @@ templates_path = ['_templates']
 language = 'de'
 locale_dirs = ['locales/']
 
+gettext_uuid = True
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
